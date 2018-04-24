9c4f2279a74ac2b31049ab310dd18c6d17e039a0
Tamás Benke <tombenke@gmail.com>
2018-04-24 10:21:17 +0200

    Add call pdms service unit test

--- src/index.spec.js
+++ src/index.spec.js
@@ -53,4 +53,66 @@ describe('pdms', () => {
             }
         })
     })
+
+    it('call pdms service', (done) => {
+        sandbox.stub(process, 'exit').callsFake((signal) => {
+            done()
+        })
+
+        const getMonitoringIsAlive = (req, cb) => {
+            cb(null, {
+                headers: {
+                    "Content-Type": "pplication/json; charset=utf-8"
+                },
+                body: {
+                    status: "OK"
+                }
+            })
+        }
+
+        const monitoringAdapter = (container, next) => {
+            // Add built-in monitoring service
+            container.pdms.add({ topic: "/monitoring/isAlive", method: "get", uri: "/monitoring/isAlive" }, function (data, cb) {
+                container.logger.info(`Monitoring handler called with ${JSON.stringify(data.request, null, '')}, ${data.method}, ${data.uri}, ...`)
+                getMonitoringIsAlive(data.request, cb)
+            })
+            next(null, {})
+        }
+
+        const adapters = [
+            npac.mergeConfig(config),
+            npac.addLogger,
+            pdms.startup,
+            monitoringAdapter
+        ]
+
+        const testPdms = (container, next) => {
+            container.logger.info(`Run job to test pdms`)
+            container.pdms.act({
+                topic: "/monitoring/isAlive",
+                method: "get",
+                uri: "/monitoring/isAlive",
+                request: {
+                    parameters: {
+                    },
+                    body: {}
+                }
+            }, (err, resp) => {
+                container.logger.info(`RES ${JSON.stringify(resp, null, '')}`)
+                next(err, resp)
+            })
+        }
+
+        const terminators = [
+            pdms.shutdown
+        ]
+
+        npac.start(adapters, [testPdms], terminators, (err, res) => {
+            if (err) {
+                throw(err)
+            } else {
+                process.kill(process.pid, 'SIGTERM')
+            }
+        })
+    })
 })
